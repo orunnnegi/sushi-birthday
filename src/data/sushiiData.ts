79e57a1b873a9adfe246dd39c47f1664ed49453e
Arun Negi <negibharat4078444@gmail.com>
2026-09-05 12:23:56 +0530

    Fix image paths and add photos

--- src/data/sushiiData.ts
+++ src/data/sushiiData.ts
@@ -459,14 +459,16 @@ export const sushiiData = {
     {
       title: 'Company',
       artist: 'just for you',
-      src: '/src/music/Company.mp3',
+      // src: '/src/music/Company.mp3',
+      src: song2,
     },
     
     {
       title: 'one more',
       artist: 'always for you',
       // src: '/music/song-03.mp3',
-      src: '/src/music/agar tum kaho.mp3',
+      // src: '/src/music/agar tum kaho.mp3',
+      src: song1,
     },
   ] as PlaylistSong[],
 
